d65000ebb88c3d249cf731331d7be1afa9822530
AhmadTubaileh <tubailehahmad82@gmail.com>
2026-01-12 13:04:15 +0200

    the new home is the store

--- Frontend/Frontend-Project/src/App.jsx
+++ Frontend/Frontend-Project/src/App.jsx
@@ -33,8 +33,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<StoreHome />} />{/*Ahmad New*/}
+        {/*<Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<StoreHome />} />{/*Ahmad New*/}
         <Route path="/store/product/:id" element={<StoreProduct />} />
         <Route path="/storeCart" element={<StoreCart />} />
         
